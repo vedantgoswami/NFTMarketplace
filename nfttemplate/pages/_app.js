b41b6287aa28ebdd5d0dbed8e2f0603a835d96ce
unknown <Vedant Goswami@LAPTOP-D32PN0D>
2022-10-10 14:01:49 +0530

    file structure changed

---
+++ nfttemplate/pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+
+import {NavBar}  from '../components/componentindex'
+
+const MyApp=({ Component, pageProps })=> (
+   <div>
+      <NavBar/>
+      <Component {...pageProps} />
+   </div>
+)
+
+export default MyApp
